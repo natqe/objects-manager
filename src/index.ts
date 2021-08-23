f6996f2bc73edc878e067f21135229f6f21123fe
Natan Farkash <natqe3@gmail.com>
2021-08-24 00:15:10 +0300

    support typings

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,32 @@ type Immutable<T> = {
     readonly [K in keyof T]: T[K] extends object ? Immutable<T[K]>: T[K]
 }
 
-export type StoreOptions<T, K extends keyof Immutable<T>> = {
+type Value<T, K extends keyof T> = ReadonlyArray<Immutable<T> & Immutable<Required<Pick<T, K>>>>
+
+export type StoreOptions<T, K extends keyof T> = {
     uniqueKey: K
-    value: ReadonlyArray<Immutable<T>>
+    value: Value<T, K>
     deepMergeArrays?: boolean | Array<string>
 }
 
-export class Store<T, K extends keyof Immutable<T>> {
-    private _value: ReadonlyArray<Immutable<T>>
+export class Store<T, K extends keyof T> {
+    private _value: Value<T, K>
     constructor(private readonly options: StoreOptions<T, K>) {
         this.dispatch(options.value)
     }
-    private readonly subscribers = Array<((value: ReadonlyArray<Immutable<T>>) => void)>()
+    private readonly subscribers = Array<((value: Value<T, K>) => void)>()
     get value() {
-        return this._value as ReadonlyArray<Immutable<T>>
+        return this._value as Value<T, K>
     }
-    private dispatch(newState: ReadonlyArray<Immutable<T>>) {
+    private dispatch(newState: Value<T, K>) {
         if (!fastDeepEqual(this.value, newState)) {
             this._value = newState.length ? deepFreeze(newState) : Object.freeze(newState)
             for (const subscriber of this.subscribers) subscriber(this.value)
         }
     }
-    upsert(value: Partial<Immutable<T>> & Required<Pick<Immutable<T>, K>> | ReadonlyArray<Partial<Immutable<T>> & Required<Pick<Immutable<T>, K>>>) {
+    upsert(value: Immutable<Partial<T>> & Immutable<Required<Pick<T, K>>> | ReadonlyArray<Immutable<Partial<T>> & Immutable<Required<Pick<T, K>>>>) {
         value = castArray(value)
-        const newState = cloneDeep(this.value) as Array<Immutable<T>>
+        const newState = cloneDeep(this.value) as Array<Immutable<T> & Immutable<Required<Pick<T, K>>>>
         for (const item of value) {
             const index = newState.findIndex(({ [this.options.uniqueKey]: _id }) => _id === item[this.options.uniqueKey] as any)
             const pathsToMerge = index !== -1 && Array.isArray(this.options.deepMergeArrays) ? this.options.deepMergeArrays.map(path => get(newState[index], path)) : []
@@ -38,24 +40,24 @@ export class Store<T, K extends keyof Immutable<T>> {
                 mergeWith(newState[index], item, (a, b) => {
                     if (Array.isArray(a) && Array.isArray(b) && (this.options.deepMergeArrays === true || pathsToMerge.includes(a))) return uniqWith([...a, ...b], fastDeepEqual)
                 }) :
-                newState.push(item as Immutable<T>)
+                newState.push(item as Immutable<T> & Immutable<Required<Pick<T, K>>>)
         }
         this.dispatch(newState)
         const effected = value.map(i => this.value.find(v => v[this.options.uniqueKey] === i[this.options.uniqueKey] as any))
         return effected
     }
-    delete(predicate?: Parameters<ReadonlyArray<Immutable<T>>['filter']>[0]) {
+    delete(predicate?: Parameters<Value<T, K>['filter']>[0]) {
         let effected = predicate ? this.value.filter(predicate) : this.value
         this.dispatch(predicate ? this.value.filter(i => !effected.includes(i)) : [])
         return effected
     }
-    subscribe(subscriber: (value: ReadonlyArray<Immutable<T>>) => void) {
+    subscribe(subscriber: (value: Value<T, K>) => void) {
         this.subscribers.push(subscriber)
         return {
             unsubscribe: () => this.unsubscribe(subscriber)
         }
     }
-    unsubscribe(subscriber: (value: ReadonlyArray<Immutable<T>>) => void) {
+    unsubscribe(subscriber: (value: Value<T, K>) => void) {
         const index = this.subscribers.findIndex(i => i === subscriber)
         if (index !== -1) this.subscribers.splice(index, 1)
     }
